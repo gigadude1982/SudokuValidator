951901d3ffbc32b813fab79634cb184f9b658147
Dalton Mangrum <dbmangrum@gmail.com>
2020-04-16 20:58:07 -0600

    Stubbed console application.

---
+++ SudokuValidator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SudokuValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sudoku Validator v1.0\n\n");
+            Console.WriteLine("Enter absolute path to input file (ex: C:\temp\sudoku.txt)...");
+
+        }
+    }
+}
